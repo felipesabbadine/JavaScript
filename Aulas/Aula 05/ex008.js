a3af83a1518fbc14d6b13173eb59c82a3fdd39c7
Felipe Sabbadine <felipesabbadine@gmail.com>
2025-10-08 16:12:37 -0300

    Condições de Javascript

---
+++ Aulas/Aula 05/ex008.js
@@ -0,0 +1,7 @@
+var din = 6600
+console.log(`Olá aqui é do banco sua conta bancaria possui ${din}`)
+
+if (din > 5000) {
+    console.log(`Você possui mais de 5000, parabens!`)
+}
+console.log(`Agradecemos pelo uso de nossos serviços`)
---
+++ Aulas/Aula 05/ex009.js
@@ -0,0 +1,8 @@
+ var alma = 29
+ console.log(`Você tem ${alma}`)
+
+ if (alma >= 33) {
+    console.log('Você pode usar um feitiço!')
+ } else {
+    console.log('Você não pode usar um feitiço, pois não tem alma.')
+ }
