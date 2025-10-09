27f43f26fd6cf53ee27d24937a7ac112c39013a5
Felipe Sabbadine <felipesabbadine@gmail.com>
2025-10-09 18:00:33 -0300

    condições if, else, else if, e switch

---
+++ Aulas/Aula 06/ex011.js
@@ -0,0 +1,13 @@
+var idade = 71
+
+console.log(`Você tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Não Vota')
+
+} else if (idade < 18 || idade > 65){
+    console.log('Voto Opcional')
+
+} else if (idade >= 18) {
+
+    console.log('Voto Obrigatorio')
+}
