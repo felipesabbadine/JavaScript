c70c006cdcff2abc527eee1d518ed3dc4c3b62c8
Felipe Sabbadine <felipesabbadine@gmail.com>
2025-11-18 17:53:29 -0300

    primeira versão do projeto sem javascript

---
+++ Desafios/Desafio 7/main.js
@@ -0,0 +1,9 @@
+function marcar() {
+    let resposta1 = document.querySelector('section#resposta')
+    let lembrar = {
+        nome: document.getElementById('nametxt'),
+        idade: document.getElementById('idadetxt'),
+    }
+
+    
+}
