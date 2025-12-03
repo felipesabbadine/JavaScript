f2eb9f58d64c6a11c6028d76202a3a1e1c49fcca
Felipe Sabbadine <felipesabbadine@gmail.com>
2025-12-03 15:23:49 -0300

    fialização do formulario

--- Desafios/Desafio 8/main.js
+++ Desafios/Desafio 8/main.js
@@ -4,10 +4,11 @@
 function enviar() {
     var nome1 = document.getElementById('nometxt').value
     var nome2 = document.getElementById('conametxt').value
+    var res = document.getElementById('res')
 
     if(nome1 === nome2){
-        window.alert('E igual')
+       res.innerHTML = `Bem-vindo ${nome2}, e bom te ter de aqui!` 
     } else {
-        window.alert('Não e igual')
+        window.alert('')
     }
 }
