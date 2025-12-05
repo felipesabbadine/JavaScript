262a4890c4b6700ec5f9d90f68c3e197cf240380
Felipe Sabbadine <felipesabbadine@gmail.com>
2025-12-05 18:06:27 -0300

    teste com função mouseover mouseout

--- Desafios/Desafio 9/main.js
+++ Desafios/Desafio 9/main.js
@@ -1,3 +1,17 @@
-document.querySelector('div.text').addEventListener('mouseover', function(
-    
-))
+/*document.querySelector('div.text').addEventListener('mouseover', function() {
+    this.style.backgroundColor = '#ffffff'
+
+    let Divs = document.querySelectorAll('.text')
+
+    Divs.style.backgroundColor = '#ffffff'
+})*/
+
+var Divs = document.querySelectorAll('.text')
+
+function hover() {
+    window.alert('oi')
+}
+
+function outhover() {
+    window.alert('tchau')
+}
