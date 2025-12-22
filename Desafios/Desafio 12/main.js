5abb88183edd538ef6fca9457e2e10a1fc24f5b7
Felipe Sabbadine <felipesabbadine@gmail.com>
2025-12-22 14:27:01 -0300

    adição do cursor ao interagir com o botão

--- Desafios/Desafio 12/main.js
+++ Desafios/Desafio 12/main.js
@@ -6,6 +6,7 @@ function interagir() {
     if(link.style.display === "block") {
         link.style.display = "none"
         link.style.transition = "ease-in-out 0.8s"
+        
     } else {
         link.style.display = "block"
         link.style.transition = "ease-in-out 0.8s"
