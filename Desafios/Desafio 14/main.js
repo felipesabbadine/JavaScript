1fafaf895193ba34de13e800b123737866db6958
Felipe Sabbadine <felipesabbadine@gmail.com>
2026-02-05 18:06:59 -0300

    começo de um menu na lateral

---
+++ Desafios/Desafio 14/main.js
@@ -0,0 +1,16 @@
+
+
+
+function abrirnav() {
+    
+ document.getElementById("sidebar").style.width = "250px"
+ document.getElementById("arrowo").style.display = "none"
+ document.getElementById("arrowc").style.display = "block"
+}
+
+function fecharnav() {
+    document.getElementById("sidebar").style.display = "20vw"
+    document.getElementById("arrowo").style.display = "block"
+    document.getElementById("arrowc").style.display = "none"
+
+}
